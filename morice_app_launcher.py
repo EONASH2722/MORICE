1b6689ac7a3c5b5525e48141733933f00d84ebd9
Janmesh <janmeshmeena10@gmail.com>
2026-04-09 16:15:13 +0530

    Fix PySide6 packaged app startup

--- morice_app_launcher.py
+++ morice_app_launcher.py
@@ -1,3 +1,25 @@
+import os
+import sys
+
+
+def _fix_pyside_paths():
+    base_dir = os.path.dirname(sys.executable)
+    internal_dir = os.path.join(base_dir, "_internal")
+    shiboken_dir = os.path.join(internal_dir, "shiboken6")
+    pyside_dir = os.path.join(internal_dir, "PySide6")
+
+    for path in (internal_dir, shiboken_dir, pyside_dir):
+        if os.path.isdir(path) and path not in sys.path:
+            sys.path.insert(0, path)
+        if os.path.isdir(path):
+            try:
+                os.add_dll_directory(path)
+            except Exception:
+                pass
+
+
+_fix_pyside_paths()
+
 from morice.pyside_app import run_app
 
 
