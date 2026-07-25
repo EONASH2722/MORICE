d862528a7c8082154366613771f8915028091f9e
Janmesh <janmeshmeena10@gmail.com>
2026-07-25 19:41:38 +0530

    fix: prefer verified direct package launcher

--- morice.cmd
+++ morice.cmd
@@ -5,13 +5,13 @@ set "ROOT=%~dp0"
 set "VNEXT_EXE=%ROOT%dist-vnext\MORICE\MORICE.exe"
 set "EXE=%ROOT%dist\MORICE\MORICE.exe"
 
-if exist "%VNEXT_EXE%" (
-  start "" "%VNEXT_EXE%"
+if exist "%EXE%" (
+  start "" "%EXE%"
   exit /b 0
 )
 
-if exist "%EXE%" (
-  start "" "%EXE%"
+if exist "%VNEXT_EXE%" (
+  start "" "%VNEXT_EXE%"
   exit /b 0
 )
 
